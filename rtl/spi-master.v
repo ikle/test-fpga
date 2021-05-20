97c88da341f92659a2d7472e291e3fae83770d99
Alexei A. Smekalkine <ikle@ikle.ru>
2021-05-20 11:19:20 +0300

    spi-master: allow to specify word size

--- rtl/spi-master.v
+++ rtl/spi-master.v
@@ -9,20 +9,22 @@
 `ifndef SPI_MASTER_V
 `define SPI_MASTER_V  1
 
-module spi_master (
+module spi_master #(
+	parameter W = 8
+)(
 	input reset, input clock,
-	input [7:0] in, output reg get, input empty,
-	output reg [7:0] out, output reg put, input full,
+	input [W-1:0] in, output reg get, input empty,
+	output reg [W-1:0] out, output reg put, input full,
 	output spi_cs_n, output reg spi_clock, output spi_mosi, input spi_miso
 );
-	localparam STOP = 9'b1_0000_0000;
+	localparam STOP = {1'b1, {W {1'b0}}};
 
 	reg avail;
-	reg [7:0] data;
-	reg [8:0] state;
+	reg [W-1:0] data;
+	reg [W:0] state;
 
-	wire stop = (state[8:0] == STOP);
-	wire last = (state[7:0] == STOP >> 1);
+	wire stop = (state[W:0]   == STOP);
+	wire last = (state[W-1:0] == STOP >> 1);
 
 	always @(posedge reset, negedge clock) #1
 		if (reset)
@@ -44,7 +46,7 @@ module spi_master (
 			end
 
 			if (stop & avail) begin
-				state <= {data[7:0], 1'b1};
+				state <= {data[W-1:0], 1'b1};
 				avail <= 0;
 			end
 
@@ -54,7 +56,7 @@ module spi_master (
 
 				if (!stop & spi_clock) begin
 					if (last & avail) begin
-						state <= {data[7:0], 1'b1};
+						state <= {data[W-1:0], 1'b1};
 						avail <= 0;
 					end
 					else
@@ -64,12 +66,12 @@ module spi_master (
 				put <= spi_clock & last;
 
 				if (!stop & !spi_clock)
-					out <= {out[6:0], spi_miso};
+					out <= {out[W-2:0], spi_miso};
 			end
 		end
 
 	assign spi_cs_n = stop;
-	assign spi_mosi = !stop & state[8];
+	assign spi_mosi = !stop & state[W];
 endmodule
 
 `endif  /* SPI_MASTER_V */
