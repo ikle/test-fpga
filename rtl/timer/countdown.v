c1ffc3d8a05b8e4879b37eed70bedf80c1df9300
Alexei A. Smekalkine <ikle@ikle.ru>
2021-05-23 02:20:00 +0300

    rtl: add countdown timer module

---
+++ rtl/timer/countdown.v
@@ -0,0 +1,29 @@
+/*
+ * Countdown Timer Module
+ *
+ * Copyright (c) 2021 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+`ifndef TIMER_COUNTDOWN_V
+`define TIMER_COUNTDOWN_V  1
+
+module countdown #(
+	parameter W = 8
+)(
+	input reset, input clock,
+	input [W-1:0] value, input put, output reg [W-1:0] count
+);
+	always @(posedge clock)
+		if (reset)
+			count <= 0;
+		else
+		if (put)
+			count <= value;
+		else
+		if (count > 0)
+			count <= count - 1;
+endmodule
+
+`endif  /* TIMER_COUNTDOWN_V */
