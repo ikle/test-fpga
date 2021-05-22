73f74fff24b0d437eadf816f0da3006810c95cd1
Alexei A. Smekalkine <ikle@ikle.ru>
2021-05-23 02:20:20 +0300

    rtl: add timeout timer module

---
+++ rtl/timer/timeout.v
@@ -0,0 +1,27 @@
+/*
+ * Timeout Timer Module
+ *
+ * Copyright (c) 2021 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+`ifndef TIMER_TIMEOUT_V
+`define TIMER_TIMEOUT_V  1
+
+`include "timer/countdown.v"
+
+module timeout #(
+	parameter W = 8
+)(
+	input reset, input clock,
+	input [W-1:0] value, input put, output full
+);
+	wire [W-1:0] count;
+
+	countdown #(W) c (reset, clock, value, put, count);
+
+	assign full = (count > 0);
+endmodule
+
+`endif  /* TIMER_TIMEOUT_V */
