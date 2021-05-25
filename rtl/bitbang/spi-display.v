337626163372787e05f3446e0cc847926241de1c
Alexei A. Smekalkine <ikle@ikle.ru>
2021-05-25 23:47:30 +0300

    rtl: add bit-bang SPI display module

---
+++ rtl/bitbang/spi-display.v
@@ -0,0 +1,31 @@
+/*
+ * SPI Display Bit-Bang Module
+ *
+ * Copyright (c) 2021 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+`ifndef BITBANG_SPI_DISPLAY_V
+`define BITBANG_SPI_DISPLAY_V  1
+
+`include "bitbang/spi-master.v"
+
+module spi_display #(
+	parameter W = 8
+)(
+	input clock, input reset, input step,
+	input dc, input [W-1:0] in, output get, input empty,
+	output spi_cs_n, output spi_clock, output spi_dc, output spi_mosi
+);
+	localparam BW = W * 2;
+
+	spi_master #(W) spi (clock, reset, step, in, get, empty, , ,
+			     spi_cs_n, spi_clock, spi_mosi, );
+
+	wire [BW-1:0] di = {BW {dc}};
+
+	bitbang #(BW) d (clock, reset, step, di, get, empty, , , 1'b0, spi_dc);
+endmodule
+
+`endif  /* BITBANG_SPI_DISPLAY_V */
---
+++ tb/spi-display-tb.v
@@ -0,0 +1,44 @@
+/*
+ * SPI Display Testbench
+ *
+ * Copyright (c) 2018-2021 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+`timescale 1ns / 100ps
+
+`include "bitbang/spi-display.v"
+`include "timer/strobe.v"
+
+`include "hello-rom.v"
+
+module tb;
+	reg clock = 0, reset;
+
+	always	# 2.5	clock = ~clock;
+
+	initial begin
+		# 10	reset <= 1;
+		# 13	reset <= 0;
+		# 5000	$finish;
+	end
+
+	wire step;
+
+	strobe #(2) t0 (clock, 2'h3, reset, 2'h0, 1'b0, step);
+
+	wire [8:0] in;
+	wire empty, get;
+	wire spi_cs_n, spi_clock, spi_dc, spi_mosi;
+
+	hello_rom #(9, "spi-display.hex", 20) rom (clock, reset, get, in, empty);
+
+	spi_display sd (clock, reset, step, in[8], in[7:0], get, empty,
+			     spi_cs_n, spi_clock, spi_dc, spi_mosi);
+
+	initial begin
+		$dumpfile ("spi-display.vcd");
+		$dumpvars ();
+	end
+endmodule
